24bfe79822a199d470372de8de74b1e1b76c2d2b
ozicreative <rozbalens@gmail.com>
2020-10-25 23:17:40 +0700

    commit

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -12,12 +12,16 @@ class Filters extends BaseConfig
 		'csrf'     => \CodeIgniter\Filters\CSRF::class,
 		'toolbar'  => \CodeIgniter\Filters\DebugToolbar::class,
 		'honeypot' => \CodeIgniter\Filters\Honeypot::class,
+		'login'      => \Myth\Auth\Filters\LoginFilter::class,
+		'role'       => \Myth\Auth\Filters\RoleFilter::class,
+		'permission' => \Myth\Auth\Filters\PermissionFilter::class,
 	];
 
 	// Always applied before every request
 	public $globals = [
 		'before' => [
-			//'honeypot'
+			'honeypot',
+			'login'
 			// 'csrf',
 		],
 		'after'  => [
@@ -34,5 +38,7 @@ class Filters extends BaseConfig
 	// List filter aliases and any before/after uri patterns
 	// that they should run on, like:
 	//    'isLoggedIn' => ['before' => ['account/*', 'profiles/*']],
-	public $filters = [];
+	public $filters = [
+		// 'login' => ['before' => ['guru']],
+	];
 }
