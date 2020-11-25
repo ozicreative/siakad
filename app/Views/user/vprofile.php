e999537da2dfd5b0d98cacfad9369f8337f3e5c7
ozicreative <rozbalens@gmail.com>
2020-11-26 00:47:59 +0700

    commit

--- app/Views/user/vprofile.php
+++ app/Views/user/vprofile.php
@@ -1,7 +1,9 @@
 <div class="row">
     <div class="col-md-12">
         <div class="row">
-            <div class="col"></div>
+            <div class="col">
+                
+            </div>
         </div>
     </div>
 </div>
